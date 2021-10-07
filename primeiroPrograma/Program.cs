2968e1a2b51c3f0cd83e9e27bd11aa25de610b53
DoglasRocha <doglas_rocha@protonmail.ch>
2021-10-07 00:10:10 -0300

    add primeiroPrograma

---
+++ primeiroPrograma/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrimeiroPrograma 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            int numberOfTimes = 5;
+
+            for (int i = 0; i < numberOfTimes; i++) Console.WriteLine($"Bem vindo ao curso de .NET {i}");
+            
+        }
+    }
+}
