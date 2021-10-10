e407506208cb7f62109fa276f32739c452c4f619
DoglasRocha <doglas_rocha@protonmail.ch>
2021-10-10 01:25:39 -0300

    write addStudent and ListStudent functions

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -6,6 +6,9 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
+            Student[] students = new Student[5];
+            int studentIndex = 0;
+
             GetUserOption(out string userOption);
 
             while (userOption.ToUpper() != "X")
@@ -13,10 +16,10 @@ namespace Revisao
                 switch (userOption)
                 {
                     case "1":
-                        // TODO: add student
+                        AddStudent(students, studentIndex);
                         break;
                     case "2":
-                        // TODO: list students
+                        ListStudents(students);
                         break;
                     case "3":
                         // TODO: calculate mean
@@ -24,25 +27,64 @@ namespace Revisao
                     default:
                         throw new ArgumentOutOfRangeException("Digite um número válido");
                 }
+                
                 GetUserOption(out userOption);
             }
         }
 
+        private static void ListStudents(Student[] students)
+        {
+            foreach(Student student in students)
+            {
+                if (!string.IsNullOrEmpty(student.Name)) 
+                    WriteLine($"ALUNO: {student.Name} - NOTA: {student.Grade}");
+            }
+        }
+
+        private static void AddStudent(Student[] students, int studentIndex)
+        {
+            Student student = new Student();
+                        
+            Write("Informe o nome do aluno: ");
+            student.Name = Console.ReadLine();
+        
+            Write("Informe a nota do aluno: ");
+
+            if (decimal.TryParse(Console.ReadLine(), out decimal grade))
+            {
+                student.Grade = grade;
+            }
+            else
+            {
+                throw new ArgumentException("Valor da nota deve ser decimal");
+            }
+
+            students[studentIndex] = student;
+            studentIndex++;
+        }
+
         private static void GetUserOption(out string userOption)
         {
+            WriteLine();
             WriteLine("Informe a opção desejada:");
             WriteLine("1 - Inserir novo aluno");
             WriteLine("2 - Listar alunos");
             WriteLine("3 - Calcular média geral");
             WriteLine("X - Sair");
-            WriteLine("");
+            WriteLine();
 
             userOption = Console.ReadLine();
+            WriteLine();
         }
 
-        static void WriteLine(string text) 
+        private static void WriteLine(string text = "") 
         {
             Console.WriteLine(text);
+        }
+
+        private static void Write(string text = "")
+        {
+            Console.Write(text);
         }
     }
 }
---
+++ Revisao/Student.cs
@@ -0,0 +1,8 @@
+namespace Revisao
+{
+    public struct Student
+    {
+        public string Name { get; set; }
+        public decimal Grade { get; set; }
+    }
+}
